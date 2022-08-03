f5270279a6d4787b29ae1c36df4adc849ed5dbe1
Asem Ashraf <73125888+LOGIC-ar@users.noreply.github.com>
2022-08-03 20:39:31 +0200

    Corrected the Doubly_Linked_List Impelementation

--- Doubly Linked List/Doubly_Linked_List.c
+++ Doubly Linked List/Doubly_Linked_List.c
@@ -1,5 +1,7 @@
 #include "Doubly_Linked_List.h"
 #define NULL ((void*)0)
+
+
 doublylinkedlist createDoublyLinkedList(){
     doublylinkedlist dll;
     dll.head = NULL;
@@ -7,52 +9,94 @@ doublylinkedlist createDoublyLinkedList(){
     dll.size = 0;
 
 }
+
+void intializeDoublyLinkedList(doublylinkedlist* pl){
+    pl->head=pl->tail=NULL;
+    pl->size=0;
+}
+
+
 int  len(doublylinkedlist* pl){
     return pl->size;
 }
+
+
 void insertLast(doublylinkedlist* pl,node* pn){
-    pl->tail->next=pn;
-    pn->prev=pl->tail;
-    pl->tail=pn;
+    if (listEmpty(pl))
+    {
+        pn->prev=pn->next=NULL;
+        pl->head=pl->tail=pn;
+    }
+    else{
+        pl->tail->next=pn;
+        pn->prev=pl->tail;
+        pl->tail=pn;
+    }
+    pl->size++;
 }
+
+
 void insertFirst(doublylinkedlist* pl,node* pn){
-    pl->head->prev=pn;
-    pn->next=pl->head;
-    pl->head=pn;
+    if (listEmpty(pl))
+    {
+        pn->prev=pn->next=NULL;
+        pl->head=pl->tail=pn;
+    }
+    else{
+        pl->head->prev=pn;
+        pn->next=pl->head;
+        pl->head=pn;
+    }
+    pl->size++;
 }
+
+
 node* deleteFirst(doublylinkedlist* pl){
     pl->head->next->prev=NULL;
     node* pOldHeadNode=pl->head;
     pl->head=pl->head->next;
     pl->head->next=NULL;
+    pl->size--;
     return pOldHeadNode;
 }
+
+
 node* deleteLast(doublylinkedlist* pl){
     pl->tail->prev->next=NULL;
     node* pOldtailNode=pl->tail;
     pl->tail=pl->tail->prev;
     pl->tail->prev=NULL;
+    pl->size--;
     return pOldtailNode;
 }
+
+
 node* deleteNodeAt(doublylinkedlist* pl,int index){
     node* pNode=getAt(pl,index); 
     pNode->prev->next=pNode->next;
     pNode->next->prev=pNode->prev;
     pNode->next=NULL;
     pNode->prev=NULL;
+    pl->size--;
     return pNode;
 }
+
+
 void insertAt(doublylinkedlist* pl,node* pn,int index){
     node* pNode = getAt(pl,index);
     pn->prev=pNode->prev;
     pn->next=pNode;
     pNode->prev->next=pn;
     pNode->prev=pn;
+    pl->size++;
 }
+
 
 void iter_seq(doublylinkedlist* pl){
 
 }
+
+
 node* getAt(doublylinkedlist* pl, int index){
     node* pNode=pl->head;
     for (int i = 0; i < index; i++)
@@ -60,4 +104,9 @@ node* getAt(doublylinkedlist* pl, int index){
         pNode=pNode->next;
     }
     return pNode;
+}
+
+
+int listEmpty(doublylinkedlist* pl){
+    return !pl->size;
 }
--- Doubly Linked List/Doubly_Linked_List.h
+++ Doubly Linked List/Doubly_Linked_List.h
@@ -13,14 +13,80 @@ typedef struct DoublyLinkedList{
     node* tail;
 }doublylinkedlist;
 
+
+/* Precondition: None
+ * Postcondition: None
+ */
 doublylinkedlist createDoublyLinkedList();
+
+
+/* Precondition: None
+ * Postcondition: None
+ */
+void intializeDoublyLinkedList(doublylinkedlist*);
+
+
+/* Precondition: The list has been intialized.
+ * Postcondition: None
+ */
 int   len(doublylinkedlist*);
+
+
+/* Precondition: None
+ * Postcondition: The element inserted at the end of the list.
+ */
 void  insertLast(doublylinkedlist*,node*);
+
+
+/* Precondition: The list must not be empty.
+ * Postcondition: The element inserted at the beginning of the list.   
+ */
 void  insertFirst(doublylinkedlist*,node*);
+
+
+/* Precondition: The list must not be empty.
+ * Postcondition: The first element is deleted.
+ *                The size of the list is decremented.
+ */
 node* deleteFirst(doublylinkedlist*);
+
+
+/* Precondition: The list must not be empty.
+ * Postcondition: The last element is deleted.
+ *                the size of the list is decremented.
+ */
 node* deleteLast(doublylinkedlist*);
+
+
+/* Precondition: The input index is less than the size of the list.
+ *               The list must not be empty.
+ * Postcondition: The element is removed from the list and returned.
+ *                The size of the list is decremented.
+ */
 node* deleteAt(doublylinkedlist*,int);
+
+
+/* Precondition: The input index is less than the size of the list.
+ * Postcondition: The node is inserted in the list at the input index.
+ *                The size of the list is incremented.
+ */
 void  insertAt(doublylinkedlist*,node*,int);
+
+
+/* Precondition: 
+ * Postcondition: 
+ */
 void  iter_seq(doublylinkedlist*);
+
+
+/* Precondition: The input index is less than the size of the list.
+ * Postcondition: None
+ */
 node* getAt(doublylinkedlist*,int);
+
+
+/* Precondition: None
+ * Postcondition: None
+ */
+int listEmpty(doublylinkedlist*);
 #endif
